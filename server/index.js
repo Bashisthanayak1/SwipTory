a431232bb064bb9f8e3543e3204207a335bb58a2
BASHISTHANAYAK <bashisthanayak09@gmail.com>
2023-12-13 00:25:46 +0530

    code has been updated in vercel.json and others 06

--- server/index.js
+++ server/index.js
@@ -22,14 +22,6 @@ app.use(
 
 
 
-// Additional headers
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Credentials', 'true'); // set 'true' for credentials
-    next();
-});
-
-
 
 dotenv.config();
 //***************** ---- ******************
